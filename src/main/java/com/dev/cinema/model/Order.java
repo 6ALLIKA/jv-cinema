c4b5741ed680cc9b8ff9b21c7559a97b9eb84af5
6ALLIKA <skorik009@gmail.com>
2020-05-27 12:30:56 +0300

    sonar

---
+++ src/main/java/com/dev/cinema/model/Order.java
@@ -0,0 +1,30 @@
+package com.dev.cinema.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime time;
+    @OneToMany
+    private List<Ticket> tickets;
+    @ManyToOne
+    private User user;
+}
